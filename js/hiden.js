8b7e88e7f9cd83c40a47f46e83f8c28c9117970c
i007c <dr007cc@gmail.com>
2020-05-22 00:00:35 +0430

    update

---
+++ js/hiden.js
@@ -0,0 +1,16 @@
+function menuToggle() {
+    var nav = document.getElementById('nav');
+    nav.classList.toggle("active");
+    var toggle = document.getElementById('toggle');
+    toggle.classList.toggle("active");
+    var mars = document.getElementById('mars');
+    mars.classList.toggle("active");
+    var github = document.getElementById('github');
+    github.classList.toggle("active");
+    var telegram = document.getElementById('telegram');
+    telegram.classList.toggle("active");
+    var discord = document.getElementById('discord');
+    discord.classList.toggle("active");
+    var instagram = document.getElementById('instagram');
+    instagram.classList.toggle("active");
+}
